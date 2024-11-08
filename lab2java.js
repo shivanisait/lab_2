4daf6d2ab6a313792106c97b9352cf9698e731ee
shivanisait <shivani.malhotra@edu.sait.ca>
2024-11-07 18:21:16 -0700

    lab 2

---
+++ lab2java.js
@@ -0,0 +1,26 @@
+BigHornSheep.addEventListener("click", function () {
+    console.log("BigHornSheep");
+    document.getElementById('image').src = "Lab 2 Image Gallery attached files Nov 7, 2024 441 PM/sheep.webp";
+    document.body.classList.add('BigHornSheep2');
+    document.body.classList.remove('BigHornSheep');
+})
+
+BlackBear.addEventListener("click", function () {
+    console.log("BlackBear");
+    document.getElementById('image').src = "Lab 2 Image Gallery attached files Nov 7, 2024 441 PM/bear.webp";
+})
+
+Moose.addEventListener("click", function () {
+    console.log("BlackBear");
+    document.getElementById('image').src = "Lab 2 Image Gallery attached files Nov 7, 2024 441 PM/moose.webp";
+})
+
+Wolf.addEventListener("click", function () {
+    console.log("BlackBear");
+    document.getElementById('image').src = "Lab 2 Image Gallery attached files Nov 7, 2024 441 PM/wolf.webp";
+})
+
+Elk.addEventListener("click", function () {
+    console.log("BlackBear");
+    document.getElementById('image').src = "Lab 2 Image Gallery attached files Nov 7, 2024 441 PM/elk.webp";
+})
